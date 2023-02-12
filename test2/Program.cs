5d7483952322a4850ad89fbbd55f6bbbab24d745
Lena <fkg@tut.by>
2023-02-12 23:38:44 +0300

    Добаленая задача4

---
+++ test2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("Первое число -> max =" + a + " > " + b);
+}
+else if (b > a)
+{
+    Console.WriteLine("Второе число -> max =" + b + " > " + a);
+}
+else
+{
+    Console.WriteLine("Второе число -> max =" + b + " = " + a);
+}
---
+++ test8/Program.cs
@@ -0,0 +1,13 @@
+﻿ // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+ Console.WriteLine("Введите число: ");
+ int N = Convert.ToInt32(Console.ReadLine());
+ int startN = 1;
+
+ while (startN <= N)
+        {
+    if (startN % 2 == 0)
+    Console.WriteLine(startN + ", ");
+    startN ++;
+        }
+        
